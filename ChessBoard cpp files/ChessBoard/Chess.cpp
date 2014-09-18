1f41dc94eaf227f7263fe57c586fb82757c81735
orlevi <orlevi1234@gmail.com>
2014-09-18 11:02:01 +0300

    unique names to the PGN files with date and time instead of the default try.pgn

--- ChessBoard cpp files/ChessBoard/Chess.cpp
+++ ChessBoard cpp files/ChessBoard/Chess.cpp
@@ -6,8 +6,10 @@
  */
 
 #include "Board.hpp"
+#include <string>
 #include <sstream>
 #include <fstream>
+#include <ctime>
 
 std::string makeAMove(Board* board, pieceColor color)
 {
@@ -59,17 +61,39 @@ int main()
 	Board* board = new Board();
 	pieceColor color = white;
 
+	/* naming the PGN file with the game date and time.  */
+	time_t t = time(0);   // get time now
+	struct tm * now = localtime( & t );
+
+	std::stringstream date, time, dateTag;
+	date << now->tm_mday << '-'
+		 << (now->tm_mon + 1) << '-'
+		 << (now->tm_year + 1900);
+	dateTag << (now->tm_year + 1900) << '.'
+			<< (now->tm_mon + 1) << '.'
+			<< now->tm_mday;
+	time << now->tm_hour <<':'
+		 << now->tm_min <<':'
+		 << now->tm_sec;
+	std::string fileName = date.str() + ' ' + time.str() + ".pgn";
+
 	/* creating the PGN file and adding the headers to the file. */
 
 	std::ofstream PGNFileStream;
-	PGNFileStream.open("try.pgn", std::ofstream::out | std::ofstream::app);
+	PGNFileStream.open(fileName.c_str(), std::ofstream::out | std::ofstream::app);
 
 	//PGNFileStream << "[Event \"testing the program\"]" << std::endl;
 	PGNFileStream << "[Site  \"Jerusalem\"]" << std::endl;
-	PGNFileStream << "[Date \"2014.24.04\"]" << std::endl;
+	PGNFileStream << "[Date \"" << dateTag.str() << "\"]" << std::endl;
 	//PGNFileStream << "[Round \"3\"]" << std::endl;
 	PGNFileStream << "[White \"Or Levi\"]" << std::endl;
 	PGNFileStream << "[Black \"Or Levi\"]" << std::endl;
+	PGNFileStream << "[Result \"*\"]" << std::endl;
+
+	/* saving the result position to update it when the game ends, the -4 is to "take back" the
+	   '*"]' in the end of the line as well as the end-line character.
+	 */
+	long resultPos = PGNFileStream.tellp() - 4;
 
 	/* handling the new game */
 
@@ -103,12 +127,17 @@ int main()
 	/* at this point there are no more legal moves, so the player whose turn it is is either losing or in a
 	   stalemate */
 
-	std::string potentialWin = (color == white) ? "# 0-1" : "# 1-0";
 	/* if there is a check, it is mate, if not - stalemate. */
-	std::string result = inCheck ? potentialWin : " 1/2-1/2";
-
+	std::string result = inCheck ? "#" : "";
 	PGNFileStream << result << std::endl;
 
+	/* updating the Result tag in the beginning of the PGN file*/
+	std::string potentialWin = (color == white) ? "0-1" : "1-0";
+	result = (inCheck ? potentialWin : "1/2-1/2") + "\"]";
+	PGNFileStream.seekp(resultPos); //setting the file position back to the result tag
+	PGNFileStream << result << std::endl;
+
+	/* writing to the PGN file is finished - closing the PGN file */
 	PGNFileStream.close();
 
 	delete board;
